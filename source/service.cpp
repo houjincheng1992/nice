de8fade52018ec21fa88a2c8a86da131a1d414fc
houjincheng <houjincheng1992@163.com>
2019-10-02 05:24:11 +0800

    commit

--- source/service.cpp
+++ source/service.cpp
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <vector>
+#include <boost/algorithm/string/join.hpp>
 
 #include <butil/time.h>
 #include "xls.h"
@@ -29,6 +30,7 @@ void NicerService::check_excel_status(
     }
 
     std::vector<std::string> titles;
+    std::vector<std::string> datas;
     std::vector<std::string> err_msg;
     std::string msg;
     for (int32_t i = 0; i < wb->sheets.count; i++) { // sheets
@@ -45,19 +47,25 @@ void NicerService::check_excel_status(
 
             // columns
             if (cols_num > titles.size()) {
-                msg = "第" + std::to_string(j) + "行数据的列数异常";
-                err_msg.emplace_back(msg)
+                msg = "第" + std::to_string(j + 1) + "行数据的列数超出标题列数，请检查";
+                err_msg.emplace_back(msg);
             }
 
             for (int32_t k = 0; k <= cols_num; k++) {
                 xls::xlsCell *cell = &row->cells.cell[k];
-                if (j == 0) {
+                if (j == 0 && cell->str != NULL) {
                     titles.emplace_back(cell->str);
+                    continue;
+                } else if (j == 0) {
+                    err_msg.emplace_back("标题格式有误");
+                    break;
+                } else if (cell->str != NULL) {
+                    datas.emplace_back(cell->str);
+                } else {
+                    msg = "第" + std::to_string(j + 1) + "行第" + std::to_string(k + 1) + "列数据异常，请检查";
+                    err_msg.emplace_back(msg);
                 }
                 
-                if (cell->str != NULL) {
-                    INFLOG << cell->str;
-                }
             }
         }
         xls::xls_close_WS(work_sheet);
@@ -65,8 +73,10 @@ void NicerService::check_excel_status(
     xls::xls_close_WB(wb);
 
     ctrl->http_response().set_content_type("application/json;charset=utf-8");
-    std::string response_str = "{\"haha\":\"nihao\"}";
-    INFLOG << "here";
+    std::string response_str = "{\"msg\":\"done\"}";
+    if (err_msg.size()) {
+        response_str = "{\"msg\":\"" + boost::algorithm::join(err_msg, ";") +"\"}";
+    }
     ctrl->response_attachment().append(response_str);
     gettimeofday(&tv1,NULL);
     INFLOG << "gettimeofday cost: " << (tv1.tv_sec - tv.tv_sec)*1000.0 + (tv1.tv_usec - tv.tv_usec)/1000.0<< "ms";
